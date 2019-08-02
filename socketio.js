9349b66c03d4d2649302df7bdc0de9a1f38056f9
Anand Kashyap <ak@cloudify.biz>
2019-08-02 17:05:30 +0530

    add users unique

--- socketio.js
+++ socketio.js
@@ -1,24 +1,30 @@
+const {addUser, removeUser, findUsers} = require('./users');
 
 const socketHandle = (io) => {
   io.on('connection', (socket) => {
-    // console.log('new client connected');
-    // socket.broadcast.emit('newClient');
 
-    socket.on('join', ({username, room}) => {
-      socket.join(room);
-      socket.broadcast.to(room).emit('newClient', `${username} has joined`);
+    socket.on('join', (options, callback) => {
+      const {error, user} = addUser({id: socket.id, ...options});
+      if (error) {
+        return callback(error);
+      }
+      socket.join(user.room);
+      socket.broadcast.to(user.room).emit('newClient', `${user.username} has joined`);
 
       socket.on('newMessage', (msg) => {
-        io.to(room).emit('newMessage', msg, username);
+        io.to(user.room).emit('newMessage', msg, user.username);
       });
 
       socket.on('sendLocation', (data) => {
-        socket.broadcast.to(room).emit('newMessage', `<a target='_blank' href='https://www.google.com/maps?q=${data.lat},${data.long}'>Location</a>`, username);
+        socket.broadcast.to(user.room).emit('newMessage', `<a target='_blank' href='https://www.google.com/maps?q=${data.lat},${data.long}'>Location</a>`, user.username);
       });
 
       socket.on('disconnect', () => {
-        io.to(room).emit('clientLeft', `${username} has left`);
+        removeUser({id: socket.id, ...options});
+        io.to(user.room).emit('clientLeft', `${user.username} has left`);
       });
+
+      callback();
     });
 
 
---
+++ users.js
@@ -0,0 +1,38 @@
+const users = [];
+
+const addUser = ({id, username, room}) => {
+  const eid = findUserIndex(username, room);
+  if (eid !== -1) {
+    return {
+      error: 'Username already in use!'
+    }
+  }
+  
+  const user = {id, username, room};
+  users.push(user);
+  return {user};
+}
+
+const removeUser = ({id, username, room}) => {
+  const eid = findUserIndex(username, room);
+  if (eid == -1) {
+    return {
+      error: 'User not present for deletion'
+    }
+  }
+  return users.splice(eid, 1)[0];
+}
+
+const findUserIndex = (username, room) => {
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+  return users.findIndex(user => user.username === username && user.room === room);
+}
+
+const findUsers = (room) => {
+  return users.filter(user => user.room === room);
+}
+
+module.exports = {
+  addUser, removeUser, findUserIndex, findUsers
+}
