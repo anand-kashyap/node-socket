f3762d434c768dbdfed9a5d4a37627f9c71c387d
Anand Kashyap <ak@cloudify.biz>
2019-08-21 11:57:53 +0530

    change error messages

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const router = express.Router({strict: true});
+const router = express.Router();
 
 const user = require('../controllers/user');
 const middleware = require('../../config/middleware');
 
 const validationArray = [
-  check('email').exists().withMessage('Email is a required value').isEmail().withMessage('must be a valid email'),
-  check('password').exists().withMessage('Password is a required value').isLength({ min: 5 }).withMessage('must be at least 5 chars long'),
+  check('email').exists().withMessage('Email is a required value').isEmail().withMessage('Email must be valid'),
+  check('password').exists().withMessage('Password is a required value').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long'),
 ];
 
 router.get('/', user.getUsers);
@@ -22,7 +22,7 @@ router.post('/authenticate',[
 ], user.authUser);
 
 router.post('/forgot-password', [validationArray[0],
-check('baseUrl').exists().withMessage('baseUrl is a required value')], user.forgotPassword);
+check('baseUrl').exists().withMessage('Base Url is a required value')], user.forgotPassword);
 
 router.post('/send-otp', [
   validationArray[0],
@@ -35,13 +35,13 @@ router.post('/confirm-otp', [
 
 router.put('/reset-password',[
   ...validationArray,
-  check('token').exists().withMessage('token is a required value')
+  check('token').exists().withMessage('Token is a required value')
 ], user.resetPassword);
 
 router.put('/update-password',[
   [middleware.checkToken],
   ...validationArray,
-  check('newPassword').exists().withMessage('New Password is a required value').isLength({ min: 5 }).withMessage('must be at least 5 chars long'),
+  check('newPassword').exists().withMessage('New Password is a required value').isLength({ min: 5 }).withMessage('New Password must be at least 5 chars long'),
 ], user.updatePassword);
 
 module.exports = router;
