a778315c49ef86eaf769c826bfe029fe047199d4
Anand Kashyap <anandkashyap60@gmail.com>
2020-05-17 10:46:06 +0000

    Express static dir added for aws

--- aws.js
+++ aws.js
@@ -42,24 +42,25 @@ socketHandle(io);
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(compression());
-app.use(cors(corsOptions));
+app.use(cors({origin: true}));
 
 app.use('/uploads', express.static(publicDirPath));
 //test db connection
-app.use('/*', function (req, res, next) {
+/*app.use('/*', function (req, res, next) {
   // console.log(mongoose.connection.readyState);
   if (mongoose.connection.readyState !== 1) {
     return res.status(500)
       .json({ success: false, message: 'Unable to connect to database' });
   }
   next();
-});
+});*/
 
 app.use('/files', files);
 app.use('/user', user);
 app.use('/room', room);
 
-app.all('/*', function (req, res, next) {
+app.use(express.static('./public'));
+app.get('/*', function (req, res, next) {
   res.sendFile('index.html', { root: __dirname + '/public' });
 });
 server.listen(port, () => console.log(`listening on http://localhost:${port}`));
