b81e555b582732d1d2a31561b897f04679f16277
Anand Kashyap <anandkashyap60@gmail.com>
2020-12-25 13:46:28 +0530

    update io config

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ const dotenv = require('dotenv').config(), //for getting env file variables
   { urlencoded, json } = require('body-parser'),
   socketio = require('socket.io');
 
+const frontUrl = process.env.ALLOWED_CORS_URL_PROD;
 const app = express(),
   server = createServer(app),
-  io = socketio(server);
+  io = socketio(server, {
+    origins: [frontUrl],
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": frontUrl,
+        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Credentials": true
+      });
+      res.end();
+    }
+  });
 
 const port = process.env.PORT || 3000,
   publicDirPath = join(__dirname, 'uploads');
@@ -26,13 +37,13 @@ const files = require('./api/routes/files'),
 const socketHandle = require('./socket/main');
 socketHandle(io);
 
-app.use(HTTPS({ trustProtoHeader: true }));
+// app.use(HTTPS({ trustProtoHeader: true }));
 
 //body-parser
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(compression());
-app.use(cors({ origin: process.env.ALLOWED_CORS_URL_PROD, credentials: true }));
+app.use(cors({ origin: frontUrl, credentials: true }));
 
 app.use('/uploads', express.static(publicDirPath));
 //test db connection
